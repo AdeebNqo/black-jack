e5803f0c826863ffd4d5229d174e02b6603fe496
Adeeb Nqo <adeebnqo@gmail.com>
2013-05-09 19:25:25 +0200

    adding some helper functions

--- my-twenty-one.scm
+++ my-twenty-one.scm
@@ -63,14 +63,66 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;  Question 3.   repeat-game takes a strategy and a number as
 ;;  arguments returns the number of games won less the number of games
-;;  lost 
-
-
+;;  lost
+(define (repeat-game strategy n)
+	(count-wins-and-loses strategy n 0 0)
+)
+(define (count-wins-and-loses strategy n current-n current-sum)
+	(if (equal? n current-n)
+		;;finished playing
+		current-sum
+		;;not finised playing
+		;;increment currrent-sum by this game's score
+		(count-wins-and-loses strategy n (+ 1 current-n) (+ current-sum (black-jack strategy)))
+	)
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Question 4.    clever
-;
 
+
+	;;Abbreviations used;;
+	;; lt - less than
+	;; get - greater or equal
+	;; so 'card-get' means card-greater-or-equal-to
+;Smarter strategy
+(define (clever customer-hand-so-far dealer-up-card)
+	;;when customer16 or less
+	(if (hand-less-than customer-hand-so-far 17)
+		;;let us check dealer for ace
+		(if (equal? (car dealer-up-card) 'A)
+			;;dealer has an A
+			#t
+			;;check if dealer's up-card is greater than 7
+			
+		)
+	)
+)
+
+;;function for determining if hand is less than a given num
+(define (hand-lt hand n)
+	(if (< (best-hand hand) n)
+		#t
+		;; when the best hand is not less than n
+		#f
+	)
+)
+;;function for determining if card is less than a given num
+(define (card-lt card n)
+	(if (< (card-min-val card) n)
+		#t
+		;;when the card is greater than n
+		#f
+	)
+)
+;;function for determining if card is greater than a num
+(define (card-get card n)
+	(if (>= (card-min-val card) n)
+		#t
+		;;when the card is less than n
+		#f
+	)
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;      Question 5.                Majority
